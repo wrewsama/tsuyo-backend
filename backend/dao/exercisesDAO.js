9edd6c8f353ab4ac03f7b82fdb54d2d5a39260f2
wrewsama <andrewlzs512@gmail.com>
2022-12-14 13:40:00 +0800

    add data access object for exercises

---
+++ backend/dao/exercisesDAO.js
@@ -0,0 +1,51 @@
+
+import mongodb from "mongodb"
+const ObjectId = mongodb.ObjectId
+let exercises
+
+export default class ExercisesDAO {
+    static async injectDB(conn) {
+        if (exercises) {
+          return
+        }
+        try {
+            exercises = await conn.db(process.env.TSUYO_NS).collection("exercises")
+        } catch (e) {
+          console.error(
+            `Unable to establish a collection handle in exercisesDAO: ${e}`,
+          )
+        }
+    }
+
+    static async getExercises({
+        filters = null,
+        page = 0,
+        exercisesPerPage = 20
+    } = {}) {
+        let query
+        if ("name" in filters) {
+            query = { $text: { $search: filters["name"] } }
+        }
+
+        let cursor
+
+        try {
+            cursor = await exercises.find(query)
+        } catch (e) {
+            console.error(`Unable to issue find command, ${e}`)
+            return { exerciseList: [], numExercises: 0 }
+        }
+
+        const displayCursor = cursor.limit(exercisesPerPage).skip(exercisesPerPage * page)
+
+        try {
+            const exerciseList = await displayCursor.toArray()
+            const numExercises = await exerciseList.countDocuments(query)
+
+            return { exerciseList, numExercises }
+        } catch (e) {
+            console.error(`Unable to convert cursor to array or problem counting documents ${e}`)
+            return { exerciseList: [], numExercises: 0 }
+        }
+    }
+}
