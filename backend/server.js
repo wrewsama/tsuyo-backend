0a4e0978ebd1917a81bbf5a63b344d0392109373
wrewsama <andrewlzs512@gmail.com>
2022-12-14 12:09:35 +0800

    create script to run server

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express"
+import cors from "cors"
+import router from "./api/tsuyo.router.js"
+
+// create express app
+const app = express()
+
+// set up middleware
+app.use(cors())
+app.use(express.json())
+
+// set up intiial routes
+app.use("api/v1/tsuyo")
+app.use("*", (req, res) => res.status(404).json({ error: "not found" }))
+
+export default app
