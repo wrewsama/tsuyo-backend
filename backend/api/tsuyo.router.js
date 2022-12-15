bacda4ecf56a882e01d375c69146b4bff3abb301
wrewsama <andrewlzs512@gmail.com>
2022-12-15 13:56:35 +0800

    edit api endpts

--- backend/api/tsuyo.router.js
+++ backend/api/tsuyo.router.js
@@ -1,12 +1,15 @@
 import express from 'express'
 import ExercisesController from "./controllers/exercises.js"
+import WorkoutsController from "./controllers/workouts.js"
 
 const router = express.Router()
 
 // set up routes
-router.route('/').get(ExercisesController.apiGetExercises)
-                 .post(ExercisesController.apiPostExercise)
-                 .delete(ExercisesController.apiDeleteExercise)
-                 .put(ExercisesController.apiUpdateExercise)
+router.route('/exercises').get(ExercisesController.apiGetExercises)
+                          .post(ExercisesController.apiPostExercise)
+                          .delete(ExercisesController.apiDeleteExercise)
+                          .put(ExercisesController.apiUpdateExercise)
+
+router.route('/workouts').post(WorkoutsController.apiPostWorkout)
 
 export default router
