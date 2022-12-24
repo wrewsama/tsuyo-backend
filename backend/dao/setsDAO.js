9b3f8d8969ef2856a28ade1a623da882e2f05878
wrewsama <andrewlzs512@gmail.com>
2022-12-24 20:29:22 +0800

    Add comments to setsDAO

--- backend/dao/setsDAO.js
+++ backend/dao/setsDAO.js
@@ -3,7 +3,23 @@ import mongodb from 'mongodb'
 
 const ObjectId = mongodb.ObjectId
 
+/**
+ * This class facilitates access to the data in the Sets collection.
+ * 
+ * It includes support for CRUD operations and getting all the sets with a
+ * given  
+ */
 export default class SetDAO {
+    /**
+     * Adds a Set document to the Sets collection.
+     * 
+     * @param {String} workoutId The id of the workout this Set belongs to.
+     * @param {String} exerciseId The id of the exercise this Set is for.
+     * @param {Number} weight The weight used for this Set.
+     * @param {Number} reps The number of repetitions performed for this Set.
+     * @returns success if the Exercise is successfully added
+     *          error otherwise
+     */
     static async addSet(workoutId, exerciseId, weight, reps) {
         try {
             const newSet = await Set.create({
@@ -22,6 +38,11 @@ export default class SetDAO {
         }
     }
 
+    /**
+     * Gets all the Sets in the database.
+     * 
+     * @returns An array containing all the Set documents in the database.
+     */
     static async getSets() {
         try {
             return await Set.find({})
@@ -31,6 +52,13 @@ export default class SetDAO {
         }
     }
 
+    /**
+     * Gets all the Sets in the database belonging to a given exercise.
+     * 
+     * @param {String} exerciseId The id of the desired exercise.
+     * @returns An array containing all the Set documents belonging to the
+     *          given exercise.
+     */
     static async getSetsByExerciseId(exerciseId) {
         try {
             const sets = await Set.find({ exerciseId: exerciseId })
@@ -41,6 +69,13 @@ export default class SetDAO {
         }
     }
 
+    /**
+     * Deletes a Set document from the database.
+     * 
+     * @param {String} setId The id of the Set to be deleted.
+     * @returns success if the Exercise is successfully added
+     *          error otherwise
+     */
     static async deleteSet(setId) {
         try {
             await Set.deleteOne({
@@ -54,6 +89,16 @@ export default class SetDAO {
 
     }
 
+    /**
+     * Updates a Set document specified by a given id with the given weight
+     * and reps.
+     * 
+     * @param {*} setId The id of the Set to be updated.
+     * @param {*} weight The new weight of the Set.
+     * @param {*} reps  The new number of reps for the Set.
+     * @returns success if the Exercise is successfully added
+     *          error otherwise
+     */
     static async updateSet(setId, weight, reps) {
         try {
             await Set.updateOne(
