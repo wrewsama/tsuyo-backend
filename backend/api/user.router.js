7172e46fa0e26681ee725bce446d9ffdef4b1d92
wrewsama <andrewlzs512@gmail.com>
2022-12-31 16:29:15 +0800

    create user routes

---
+++ backend/api/user.router.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const userRouter = express.Router()
+
+// login
+router.post('/login', () => {})
+
+// signup
+router.post('/signup', () => {})
+
+export default userRouter 
