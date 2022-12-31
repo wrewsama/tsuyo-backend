838fd249a06cf798089286683d85d0a8782f339e
wrewsama <andrewlzs512@gmail.com>
2022-12-31 17:03:18 +0800

    add user DAO

---
+++ backend/dao/userDAO.js
@@ -0,0 +1,11 @@
+import User from "../models/user.js"
+
+export default class UserDAO {
+    static async loginUser() {
+        // TODO
+    }
+
+    static async signupUser() {
+        // TODO
+    }
+}
