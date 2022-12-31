85780e8159c05f15c489a8bd1f0564a69c96618b
wrewsama <andrewlzs512@gmail.com>
2022-12-31 19:43:07 +0800

    add json web tokens

--- backend/dao/userDAO.js
+++ backend/dao/userDAO.js
@@ -1,6 +1,12 @@
 import User from "../models/user.js"
+import jwt from "jsonwebtoken"
 
 export default class UserDAO {
+
+    static createToken(_id) {
+        return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '3d' })
+    }
+
     static async loginUser() {
         // TODO
     }
@@ -8,8 +14,9 @@ export default class UserDAO {
     static async signupUser(email, password) {
         try {
             const user = await User.signup(email, password)
+            const token = this.createToken(user._id)
 
-            return { email, user }
+            return { email, token }
         } catch (e) {
             console.error(`Unable to signup: ${e}`)
             return { error: e }
