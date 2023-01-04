f2e657e16b108fc5359eb794248df57b1fa5cd73
wrewsama <andrewlzs512@gmail.com>
2023-01-04 16:07:07 +0800

    add comments for middleware

--- middleware/requireAuth.js
+++ middleware/requireAuth.js
@@ -1,8 +1,14 @@
 import jwt from 'jsonwebtoken'
 import User from "../models/user.js"
 
+/**
+ * Ensures the http requests to the backend are authorised.
+ * 
+ * Checks the json web token in the request's headers and ensures that
+ * the token corresponds to an existing user in the database.
+ */
 const requireAuth = async (req, res, next) => {
-    // verification
+    // get auth token
     const { authorization } = req.headers
 
     if (!authorization) {
@@ -12,6 +18,7 @@ const requireAuth = async (req, res, next) => {
     const token = authorization.split(' ')[1]
 
     try {
+        // check if token is valid
         const { _id } = jwt.verify(token, process.env.SECRET)
 
         req.user = await User.findOne({ _id }).select('_id')
