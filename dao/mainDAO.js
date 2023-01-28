745ba7715581818522e0d6b6e852786eda995c07
wrewsama <andrewlzs512@gmail.com>
2023-01-28 14:07:06 +0800

    add mainDAO to encapsulate the other DAO

---
+++ dao/mainDAO.js
@@ -0,0 +1,13 @@
+import ExercisesDAO from "./exercisesDAO";
+import SetsDAO from "./setsDAO";
+import userDAO from "./userDAO";
+import workoutsDAO from "./workoutsDAO";
+
+const mainDAO = {
+    ExercisesDAO,
+    SetsDAO,
+    userDAO,
+    workoutsDAO
+}
+
+export default mainDAO
