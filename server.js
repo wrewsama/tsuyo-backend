da38e25542c39bf79dfc32c9f54c6dff6f0eb060
wrewsama <andrewlzs512@gmail.com>
2023-01-28 14:15:31 +0800

    refactor index and server for dependency injection

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 import express from "express"
 import cors from "cors"
-import router from "./api/tsuyo.router.js"
-import userRouter from "./api/user.router.js"
+import makeRouter from "./api/tsuyo.router.js"
+import makeUserRouter from "./api/user.router.js"
 
-// create express app
-const app = express()
-
-// set up middleware
-app.use(cors())
-app.use(express.json())
-
-// set up intiial routes
-app.use("/api/v1/tsuyo", router)
-app.use("/api/v1/user", userRouter)
-app.use("*", (req, res) => res.status(404).json({ error: "not found" }))
-
-export default app
+const makeApp = (mainDao) => {
+    // create express app
+    const app = express()
+    
+    // set up middleware
+    app.use(cors())
+    app.use(express.json())
+    
+    // set up intiial routes
+    app.use("/api/v1/tsuyo", makeRouter(
+        mainDao.exercisesDAO,
+        mainDao.workoutsDAO,
+        mainDao.setsDAO))
+    app.use("/api/v1/user", makeUserRouter(mainDao.setsDAO))
+    app.use("*", (req, res) => res.status(404).json({ error: "not found" }))
+    
+    return app
+}
+export default makeApp
