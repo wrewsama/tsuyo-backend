08ba47b00ccb22152ba0d1a14b85afed54dda332
wrewsama <andrewlzs512@gmail.com>
2023-01-28 13:56:49 +0800

    refactor router for dependency injection

--- api/tsuyo.router.js
+++ api/tsuyo.router.js
@@ -5,34 +5,47 @@ import SetsController from "./controllers/sets.js"
 import requireAuth from '../middleware/requireAuth.js'
 
 /**
- * This router defines the API routes used by tsuyo.
+ * Creates a router that defines the API routes used by tsuyo.
+ * 
+ * @param {Class} exercisesDao Data Access Object for the exercises DB
+ * @param {Class} workoutsDao Data Access Object for the workouts DB
+ * @param {Class} setsDao Data Access Object for the sets DB
+ * @returns router
  */
+const makeRouter = (exercisesDao, workoutsDao, setsDao) => {
+    const router = express.Router()
 
-const router = express.Router()
+    // initialise controllers
+    const exercisesController = ExercisesController(exercisesDao)
+    const setsController = SetsController(setsDao)
+    const workoutsController = WorkoutsController(workoutsDao)
+    
+    // set up authorisation middleware
+    router.use(requireAuth)
+    
+    // Exercises routes
+    router.route('/exercises').get(exercisesController.apiGetExercises)
+                              .post(exercisesController.apiPostExercise)
+                              .delete(exercisesController.apiDeleteExercise)
+                              .put(exercisesController.apiUpdateExercise)
+    
+    router.route('/exercises/:id').get(exercisesController.apiGetExerciseById)
+    
+    // Workouts routes
+    router.route('/workouts').post(workoutsController.apiPostWorkout)
+                             .get(workoutsController.apiGetWorkouts)
+                             .delete(workoutsController.apiDeleteWorkout)
+    
+    router.route('/workouts/:id').get(workoutsController.apiGetWorkoutById)
+    
+    // Sets routes
+    router.route('/sets').post(setsController.apiPostSet)
+                         .get(setsController.apiGetSets)
+                         .delete(setsController.apiDeleteSet)
+                         .put(setsController.apiUpdateSet)
+    
+    router.route('/sets/:eid').get(setsController.apiGetSetsByExerciseId)
+    return router
+}
 
-// set up authorisation middleware
-router.use(requireAuth)
-
-// Exercises routes
-router.route('/exercises').get(ExercisesController.apiGetExercises)
-                          .post(ExercisesController.apiPostExercise)
-                          .delete(ExercisesController.apiDeleteExercise)
-                          .put(ExercisesController.apiUpdateExercise)
-
-router.route('/exercises/:id').get(ExercisesController.apiGetExerciseById)
-
-// Workouts routes
-router.route('/workouts').post(WorkoutsController.apiPostWorkout)
-                         .get(WorkoutsController.apiGetWorkouts)
-                         .delete(WorkoutsController.apiDeleteWorkout)
-
-router.route('/workouts/:id').get(WorkoutsController.apiGetWorkoutById)
-
-// Sets routes
-router.route('/sets').post(SetsController.apiPostSet)
-                     .get(SetsController.apiGetSets)
-                     .delete(SetsController.apiDeleteSet)
-                     .put(SetsController.apiUpdateSet)
-
-router.route('/sets/:eid').get(SetsController.apiGetSetsByExerciseId)
-export default router
+export default makeRouter
