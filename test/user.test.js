bde5f5ef76b651e5f7ced77cadedbe46c3a47d9f
wrewsama <andrewlzs512@gmail.com>
2023-01-28 16:34:58 +0800

    add login tests

--- test/user.test.js
+++ test/user.test.js
@@ -48,3 +48,47 @@ describe("Sign Up", () => {
         expect(response.statusCode).toBe(200)
     })
 })
+
+describe("Log In", () => {
+    beforeEach(() => {
+        mainDAO.usersDao.loginUser.mockReset()
+    })
+    test("Should call the login function once", async () => {
+        const response = await request(app).post('/api/v1/user/login/').send({
+            email: "b@a.com",
+            password: "a"
+        })
+        expect(mainDAO.usersDao.loginUser.mock.calls.length).toBe(1)
+    })
+
+    test("Should correctly take in the given email and password", async () => {
+        const response = await request(app).post('/api/v1/user/login/').send({
+            email: "b@a.com",
+            password: "a"
+        })
+        expect(mainDAO.usersDao.loginUser.mock.calls[0][0]).toBe("b@a.com")
+        expect(mainDAO.usersDao.loginUser.mock.calls[0][1]).toBe("a")
+    })
+
+    test("Should respond with json object containing email", async () => {
+        mainDAO.usersDao.loginUser.mockResolvedValue({
+            email: "b@a.com"
+        })
+        const response = await request(app).post('/api/v1/user/login/').send({
+            email: "b@a.com",
+            password: "a"
+        })
+        expect(response.body.email).toBe("b@a.com")
+    })
+
+    test("Should respond with a 200 status code", async () => {
+        mainDAO.usersDao.loginUser.mockResolvedValue({
+            email: "a"
+        })
+        const response = await request(app).post('/api/v1/user/login/').send({
+            email: "b@a.com",
+            password: "a"
+        })
+        expect(response.statusCode).toBe(200)
+    })
+})
